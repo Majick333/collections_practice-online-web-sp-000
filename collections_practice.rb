8dc5413c6f09767dabba97ceebde0931bf88efbc
Ryan M <rmccauleymusic@gmail.com>
2019-10-31 00:48:32 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -16,3 +16,16 @@ def sort_array_desc(array)
     end
   end
 end
+
+def sort_array_char_count(array)
+  array.sort do |a,b|
+    if a.length > b.length
+      -1
+    elsif a.length < b.length
+      1
+    elsif a == begin
+      0  
+    end
+  end
+end
+      
